cd85a4105577c97b7fbe82eeeed7f5beb9b04d56
Gabriel Leandro <gabriel.sousa@dock.tech>
2021-11-23 15:27:48 -0300

    [develop] Adjusting memoization v1 and v2

--- other_topics/memoization/v2/utils/memoize.py
+++ other_topics/memoization/v2/utils/memoize.py
@@ -6,5 +6,6 @@ class Memoize:
 
     def __call__(self, *args):
         if args not in self.memo:
+            print(f'Return from function {self.fn.__name__}() not in cache, executing now with args {list(args)}')
             self.memo[args] = self.fn(*args)
         return self.memo[args]
